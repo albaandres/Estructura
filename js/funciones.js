4c6f6d4f20d308e0dc5774e36996d5ca57a279bb
albaandres <albam.andben@educa.jcyl.es>
2021-09-27 10:27:12 +0200

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+alert ("Inicio")
+window.onlad=function(){
+
+}
+let elemento = document.getElementById ("nombre");
+elemento.style.color="red";
+ 
+elemento.addEventListener("mouseover", function () {
+ 
+    elemento.style.color = "green";
+    elemento.innerText = (elemento.innerText).toUpperCase();
+ 
+}, false)
+elemento.addEventListener("mouseout", function () {
+    elemento.style.color = "black";
+    elemento.innerText = (elemento.innerText).toLowerCase();
+ 
+}, false)
